8734bec08aff409d7f09178be1a90da5ea64895e
Hava <avivaviv11223344@gmail.com>
2025-10-05 18:17:42 +0300

    Adjusted drivetrain opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricTele.java
@@ -1,42 +1,50 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name="FieldCentricTeleop", group="Teleop")
 public class FieldCentricTele extends LinearOpMode {
-    private DcMotor frontLeft, frontRight, backLeft, backRight;
+    private DcMotor leftFront, rightFront, leftBack, rightBack;
     private final double SLOW_MODE = 0.3;
     private final double FAST_MODE = 1;
     private double modeCheck=1; // can prob not init but its better to start automatically in fast
 
-    private BNO055IMU imu;
+    private IMU imu;
 
     public void initMotors(){
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack   = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
     }
 
     public void initIMU(){
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameter = new BNO055IMU.Parameters();
-        parameter.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameter = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+                        )
+        );
         imu.initialize(parameter);
     }
 
     @Override
     public void runOpMode() throws InterruptedException {
+        initIMU();
         initMotors();
+
 
         waitForStart();
         while(opModeIsActive()){
@@ -52,12 +60,11 @@ public class FieldCentricTele extends LinearOpMode {
             double spdx = gamepad1.left_stick_x  * modeCheck;
             double rotate = gamepad1.right_stick_x * modeCheck;
 
-            Orientation angles = imu.getAngularOrientation();
-            double heading = angles.firstAngle;
+            double heading = -imu.getRobotYawPitchRollAngles().getYaw();
 
-
-            double cosAngle = Math.toRadians(Math.cos(heading)); //better like this to show telemetry in degrees(?)
-            double sinAngle = Math.toRadians(Math.sin(heading));
+            double headingRad = Math.toRadians(heading); // so you can keep seeing degrees on telemetry
+            double cosAngle = Math.cos(headingRad);
+            double sinAngle = Math.sin(headingRad);
 
 
             double fX = spdx * cosAngle - spdy * sinAngle;
@@ -70,10 +77,10 @@ public class FieldCentricTele extends LinearOpMode {
             double frPower = (fY - fX - rotate)/mathdenom;
             double brPower = (fY + fX  - rotate)/mathdenom;
 
-            frontLeft.setPower(flPower);
-            backLeft.setPower(blPower);
-            frontRight.setPower(frPower);
-            backRight.setPower(brPower);
+            leftFront.setPower(flPower);
+            leftBack.setPower(blPower);
+            rightFront.setPower(frPower);
+            rightBack.setPower(brPower);
 
             telemetry.addData("heading:", heading);
             if(modeCheck == 1.0){  //just for convenience to see actual mode
